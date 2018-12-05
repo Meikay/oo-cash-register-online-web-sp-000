67b1563bd62188434eee3f704402aaff938443c8
Meisuleen Kased <meisuleen@gmail.com>
2018-12-05 03:58:22 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,31 @@
+require 'pry'
 
+# Add items of varying quantities and prices
+# Calculate discounts
+# Keep track of what's been added to it
+# Void the last transaction
+
+#discount = price - 20
+
+
+
+class CashRegister
+  attr_accessor :total, :discount, :price, :items
+
+def initialize(discount = 0)
+  @total = 0
+  @discount = 20
+  items = []
+end
+
+def add_item(title, price)
+  @price = price
+  @total = price += price
+
+  end
+
+
+
+
+
+end
